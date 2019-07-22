0492eaab6a918ded9e8bef74cf1c1061110e1ac5
LazarievEd <lazarev223@gmail.com>
2019-07-22 05:30:49 +0300

    add dz18

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-function sortWords(str) {
-    var sharedWords = str.split(' ');
-    var sharedWordsAndLetters = [];
-    for(var i = 0; i < sharedWords.length; i++){
-        sharedWordsAndLetters[i] = sharedWords[i].split('');
-        sharedWordsAndLetters[i].sort();
-        sharedWordsAndLetters[i] = sharedWordsAndLetters[i].join('');
+function splitArr(str){
+    return str.split(' ');
+}
+function searchNumber(say){
+    for(var i = 0; i < say.length; i++){
+        if (!isNaN(+say[i])){
+            return say[i];
+        }
     }
-
-    var arrObj = [];
-
-        for (var i = 0; i < sharedWords.length; i++){
-            arrObj.push({key1 : sharedWords[i], key2 : sharedWordsAndLetters[i]})
-    }
-
-    arrObj.sort (function (a,b){
-        if (a.key2 < b.key2) return -1;
+}
+function mainSort(str){
+    var sortArr = splitArr(str);
+    sortArr.sort(function(a, b){
+        return searchNumber(a) - searchNumber(b);
     });
-
-    var getSort = [];
-
-    for (var i = 0; i < arrObj.length; i++){
-        getSort.push (arrObj[i].key1);
-    }
-
-    return getSort.join(' ');
+    return sortArr.join(' ');
 }
 
-console.log(sortWords('A3lice 2Bob Er1nst')); // 'Er1nst 2Bob A3lice';
-console.log(sortWords('2Fiona Ig7or J1amila B3ob Ali5ce')); // 'J1amila 2Fiona B3ob A3lice Ig7or';
-console.log(sortWords('Tes1t')); // 'Tes1t';
-
+console.log (mainSort('A3lice 2Bob Er1nst'));
+console.log (mainSort('2Fiona Ig7or J1amila B3ob Ali5ce'));
+console.log (mainSort('Tes1t'));
